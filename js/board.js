b40ed7500872afaef75f3a708b78f03ba698275f
Davix76 <30471850+Davix76@users.noreply.github.com>
2017-08-30 18:27:08 +0200

    cambios dia 2

--- js/board.js
+++ js/board.js
@@ -1,15 +1,22 @@
-function Board (x,y) {
-  this.black= x;
-  this.white = y;
-  this.board = [
-  [null,null,null,null,null,null,null,null],
-  [null,null,null,null,null,null,null,null],
-  [null,null,null,null,null,null,null,null],
-  [null,null,null,x,y,null,null,null],
-  [null,null,null,y,x,null,null,null],
-  [null,null,null,null,null,null,null,null],
-  [null,null,null,null,null,null,null,null],
-  [null,null,null,null,null,null,null,null],
-  ];
+function Board(lado) {
+  this.element = $("#game");
 
+  this.scoreA = 0;
+  this.scoreB = 0;
+
+  // Create the board
+  this.board = undefined;
+  for(var i=0; i < lado ;i++){
+    this.board.push([]);
+    for(var j=0; j< lado; j++){
+      var ficha = $("<div class='ficha' data-status='vacia'>");
+      ficha.appendTo(this.element);
+      this.board[j].push(ficha);
+    }
+    this.element.append($('<br>'));
+  }
+}
+
+Board.prototype.changeFicha = fucntion(color){
+  
 }
--- js/index.js
+++ js/index.js
@@ -1,25 +1,4 @@
 
 $(document).ready(function() {
-var player1 = new Player();
-var player2 = new Player();
-var board = new Board (player1,player2);
-console.log(board);
-
-  var game = $("#game");
-
-  // Filas y columnas
-  var m = 8;
-
-  for(var i = 0; i < m; i++){
-    for(var j= 0; j < m; j++){
-    var ficha = $("<div class='ficha'>");
-    ficha.appendTo(game);
-    }
-  //  var ficha = new Ficha(game);
-    game.append($('<br>'));
-  }
-  $(".ficha").on("click", function (){
-    
-  });
-
+  var board = new Board (8);
 });
